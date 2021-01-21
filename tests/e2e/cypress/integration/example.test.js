5461f24ff53e87880744540bf72a9a64af5fa0f3
Valentyn Kremeshnyi <valentyn.kremeshnyi@saucelabs.com>
2021-01-21 13:46:17 -0800

    update test

--- tests/e2e/cypress/integration/example.test.js
+++ tests/e2e/cypress/integration/example.test.js
@@ -1,12 +1,10 @@
-import { Selector } from 'testcafe';
-fixture `Getting Started`
-    .page `http://devexpress.github.io/testcafe/example`
-
-const testName = 'testcafe test'
-test(testName, async t => {
-    await t
-        .typeText('#developer-name', 'devx')
-        .click('#tried-test-cafe')
-        .click('#submit-button')
-        .expect(Selector('#article-header').innerText).eql('Thank you, devx!');
-});
+context('Actions', () => {
+    beforeEach(() => {
+        cy.visit('https://example.cypress.io/commands/actions')
+    })
+    it('.type() - type into a DOM element', () => {
+        // https://on.cypress.io/type
+        cy.get('.action-email')
+            .type('fake@email.com').should('have.value', 'fake@email.com')
+    })
+})
